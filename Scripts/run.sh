d294bf32cfef6ce52d753d724dc1c9c5d21d5dee
ReshmiDeb27 <Rdeb@unomaha.edu>
2025-05-12 00:42:50 -0500

    Update run.sh

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Step 1: Install required Python packages
+# Step 1: Update pip and install necessary project dependencies from requirements.txt for dashboard
 pip3 install --quiet -r ../requirements.txt
 pip3 install --upgrade pip
 
-# Step 2: Prepare input and output directories
+# Step 2: Create input/output folders to support data flow and file management
 mkdir -p ../input ../output
 
-# Step 3: Copy CSVs from Data to input
+# Step 3: Place data files in the input path to prepare for analysis
 cp ../Data/person.csv ../input/
 cp ../Data/encounter.csv ../input/
 cp ../Data/outcome.csv ../input/
 
-# Step 4: Run Python script to generate summary data
+# Step 4: Run the script in the input files to preprocess raw data and produce cleaned data for further use
 echo "Running data processing script"
 python3 process_data.py ../input/person.csv ../input/encounter.csv ../input/outcome.csv ../output/summary.csv
 
-# Step 5: Load summary.csv to SQL database
+# Step 5: Load the cleaned and processed data into the MySQL database for storage and querying
 echo "Loading summary.csv into database..."
 python3 Scripts/load_data.py
 
@@ -24,7 +24,7 @@ echo "summary.csv successfully loaded into MySQL"
 
 echo "Data pipeline complete. Launching Streamlit dashboard..."
 
-# Step 7: Run Streamlit
+# # Step 6: Launch the interactive dashboard to review metrics and trends
 cd ..
 streamlit run app.py
 
